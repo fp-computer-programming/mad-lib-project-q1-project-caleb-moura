39a61c6dbe43bafd060f10a5b6a20834fbe3f47a
calebmoura <144838461+calebmoura@users.noreply.github.com>
2023-11-02 23:11:29 -0400

    Add files via upload

---
+++ mad_libs_q1.py
@@ -0,0 +1,115 @@
+# This is an introduction to the game, the "preliminary" game
+# Ask for user input
+print("This first mad lib is a intro to the game")
+noun1 = input("Enter a noun: ")
+adjective1 = input("Enter an adjective: ")
+verb1 = input("Enter a verb: ")
+adverb1 = input("Enter an adverb: ")
+noun2 = input("Enter another noun: ")
+adjective2 = input("Enter another adjective: ")
+verb2 = input("Enter another verb: ")
+adverb2 = input("Enter another adverb: ")
+noun3 = input("Enter one more noun: ")
+adjective3 = input("Enter one more adjective: ")
+
+# Display the story
+print(f"Once upon a time, there was a {noun1}.")
+print(f"The {noun1} was very {adjective1}")
+print(f"This {noun1} loved to")
+print(f"{verb1} {adverb1} in the forest.")
+print(f"One day, {noun1} met a {adjective2}")
+print(f"{noun1} and encounters {noun2}")
+print(f"{noun2} could {verb2} {adverb2}.")
+print(f"They became fast friends and went on many {adjective3} adventures together.")
+print(f"The end.")
+print(f"Congrats on compeleting the intro")
+
+# Mad Lib Game
+
+# Initialize a list to store user inputs
+user_inputs = []
+
+# This is the start to the actual game
+# Prompt user for inputs
+user_inputs0 = (input("Enter a noun: "))
+user_inputs1 = (input("Enter an adjective: "))
+user_inputs2 = (input("Enter a type of monster: "))
+user_inputs3 = (input("Enter a verb: "))
+user_inputs4 = (input("Enter an adverb that associates with the previous verb: "))
+user_inputs5 = (input("Enter a plural noun: "))
+user_inputs6 = (input("Enter a color: "))
+user_inputs7 = (input("Enter a place: "))
+user_inputs8 = (input("Enter an animal: "))
+user_inputs9 = (input("Enter a verb ending in 'ing': "))
+user_inputs10 = (input("Enter a food: "))
+user_inputs11 = (input("Enter a number: "))
+user_inputs12 = (input("Enter a body part: "))
+user_inputs13 = (input("Enter an emotion: "))
+user_inputs14 = (input("Enter a type of clothing: "))
+user_inputs15 = (input("Enter a type of vehicle: "))
+user_inputs16 = (input("Enter a type of music: "))
+user_inputs17 = (input("Enter a type of sport: "))
+user_inputs18 = (input("Enter a type of weather: "))
+user_inputs19 = (input("Enter a type of object than can contain something: "))
+user_inputs20 = (input("Enter a type of drink: "))
+user_inputs21 = (input("Enter a type of plant: "))
+user_inputs22 = (input("Enter a type of instrument: "))
+user_inputs23 = (input("Enter a type of hobby: "))
+user_inputs24 = (input("Enter a type of dessert: "))
+user_inputs25 = (input("Enter a type of fruit: "))
+user_inputs26 = (input("Enter a place for shelter: "))
+user_inputs27 = (input("Enter a type of smell: "))
+user_inputs28 = (input("Enter a type of sound: "))
+user_inputs29 = (input("Enter a type of taste: "))
+user_inputs30 = (input("Enter a type of texture: "))
+user_inputs31 = (input("Enter the name of a magic attack"))
+user_inputs32 = (input("Enter a adjective expressing fear"))
+user_inputs33 = (input("Enter onamatopoeia expressing relief"))
+user_inputs34 = (input("Enter a name for the two friends to go by"))
+user_inputs35 = (input("Enter an adjective to show the monster's fierceness"))
+user_inputs36 = (input("Enter an adjective to show the monster is defeated"))
+user_inputs37 = (input("Enter the name of a food place"))
+user_inputs38 = (input("Enter a celebrity name: "))
+
+# Create a Mad Lib story using user inputs
+mad_lib_story = f"In a distant land, there was a {user_inputs[0]} who spent their days living in a {user_inputs[7]}. " \
+                f"This {user_inputs[0]} was known for their {user_inputs[1]} and ability to make the best from the worst." \
+                f"Although they had a nice life, their hats weren't looking as {user_inputs[6]} as they normally do." \
+                f"{user_inputs[0]} wanted to do something more." \
+                f"{user_inputs[0]} didn't seek fame, or to be the next {user_inputs[38]}." \
+                f"Therefore, {user_inputs[0]} decides that they are going to set off on an adventure." \
+                f"In preperation for this, a bunch of {user_inputs[10]} was stored." \
+                f"Finally, the journey begins" \
+                f"Coming across a desert, our protagonist encounters {user_inputs[11]} {user_inputs[5]}." \
+                f"The {user_inputs[5]} help him to cross the desert." \
+                f"All of the walking leaves our protagonist with an aching {user_inputs[12]} " \
+                f"Luckily, there is a {user_inputs[13]} doctor who helps him." \
+                f"As a parting gift, he is given a {user_inputs[14]}" \
+                f"All Healed up, the adventure now extends to the woods" \
+                f"While adventuring through the woods, our protagonist {user_inputs[0]} befriends a {user_inputs[8]}." \
+                f"The two become great friends and continue to jounrey the world together." \
+                f"The {user_inputs[8]} was named {user_inputs[6]}." \
+                f"{user_inputs[6]} could play the {user_inputs[22]}, specifically {user_inputs[16]} music." \
+                f"The middle of the adventure was quite random, but it built up quickly." \
+                f"They began to {user_inputs[23]} in their free time." \
+                f"One day, while {user_inputs[9]}, they stumbled upon a {user_inputs[26]}" \
+                f"The timing was quite convenient as they needed to gain cover from the {user_inputs[18]}" \
+                f"a hidden {user_inputs[19]} filled with {user_inputs[20]} and {user_inputs[21]}." \
+                f"These items gave off quite the {user_inputs[27]} aroma" \
+                f"Although the smell was enticing, the {user_inputs[28]} was quite eerie." \
+                f"Taking a bite from the herbs, a {user_inputs[29]} taste was released" \
+                f"This causes trouble for them as they anger the owner of these items" \
+                f"{user_inputs[2]} {user_inputs[9]} {user_inputs[4]} while {user_inputs[3]}." \
+                f"The {user_inputs[35]} {user_inputs[2]} lunges towards them" \
+                f"Our protagonist is not unexperienced in battle, as he somehow knows magic." \
+                f"Using his {user_inputs[31]} he defeats the monster" \
+                f"The monster's {user_input[30]} skin didnt look as threatening anymore as he laid vulnerable." \
+                f"The {user_inputs[2]} is {user_inputs[36]}." \
+                f"The two {user_inputs[34]} have defeated the {user_inputs[2]}." \
+                f"The {user_inputs[2]} gives them a {user_inputs[25]} out of respect for defeating him" \
+                f"This may have also been a plead for it's life, as it looked {user_inputs[32]}." \
+                f"Extremely tired from the battle, the two rent a {user_inputs[15]} for the journey back home." \
+                f"On their way home they stopped by the {user_inputs[37]} and enjoyed some {user_inputs[24]}. " \
+                f"{user_inputs[33]} the adventure has come to an end, a quite happy end." \
+                f"The two settled down, and spent the rest of the day relaxing, and playing {user_inputs[17]}." \
+                f"The end."
